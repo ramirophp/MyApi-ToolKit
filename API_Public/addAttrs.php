de71878f448eeafb2e4f9241ffd7a94c1c03d08f
rgg7888 <rgg7888@outlook.com>
2021-07-01 15:14:27 -0600

    agregue el segundo formulario en php

--- API_Public/addAttrs.php
+++ API_Public/addAttrs.php
@@ -1,3 +1,102 @@
+<?php
+
+require_once '../API_Build/Html.php';
+require_once '../API_Build/Tags.php';
+
+doctype();
+html([
+    head([
+        linx([
+            'rel' => 'shorcut icon',
+            'type' => 'image/png',
+            'href' => './assets/favicon.png'
+        ]),
+        linx([
+            'rel' => 'stylesheet',
+            'type' => 'text/css',
+            'href' => '../API_View/loader.css'
+        ]),
+        title("Add Attrs POST")
+    ]),
+    body([
+        ul(
+            li(a("home",[
+                'attrs' => [
+                    'href' => './index.php'
+                ]
+            ]))
+        ),
+        h2("ADD Html Atributes",[
+            'attrs' => [
+                'id' => 'up'
+            ]
+        ]),
+        section([
+            section(
+                form([
+                    label([
+                        span("ADD ATTR : "), 
+                        input([
+                            'type' => "text",
+                            'name' => "attr",
+                            'id' => "attr",
+                            'placeholder' => "add attr"
+                        ])
+                    ],[
+                        'attrs' => [
+                            'for' => 'tag'
+                        ]
+                    ]),
+                    div(null,[
+                        'attrs' => [
+                            'id' => 'result'
+                        ]
+                    ]),
+                    input([
+                        'type' => "submit",
+                        'value' => "SAVE"
+                    ])
+                ])
+            )
+        ],[
+            'attrs' => [
+                'class' => 'paginacion'
+            ]
+        ]),
+        footer([
+            "WELCOME TO &copy; futuredeveloper.xyz",
+            a(i(null,[
+                'attrs' => [
+                    'class' => 'fas fa-arrow-circle-up upIcon'
+                ]
+            ]),['attrs'=>[
+                'href' => '#up'
+            ]])
+        ]),
+        script("var recurso = 'attrs';"),
+        script(null,[
+            'attrs' => [
+                'src' => '../API_View/post_dato.js',
+            ]
+        ]),
+        script(null,[
+            'attrs' => [
+                'src' => 'https://kit.fontawesome.com/60163d706b.js',
+                '!' => 'defer',
+                'crossorigin' => 'anonymous'
+            ]
+        ])
+    ],[
+        'js' => true
+    ])
+],[
+    'js' => true,
+    'attrs' => [
+        'lang' => 'es'
+    ]
+]);
+
+/*
 <!DOCTYPE html>
 <html>
     <head>
@@ -30,3 +129,4 @@
         <script src="../API_View/post_dato.js"></script>
     </body>
 </html>
+*/
