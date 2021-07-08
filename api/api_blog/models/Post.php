16751dfb025a3c387c7471edb153664003a02a40
rgg7888 <rgg7888@outlook.com>
2021-07-08 13:35:28 -0600

    method post, create a post

--- api/api_blog/models/Post.php
+++ api/api_blog/models/Post.php
@@ -75,4 +75,36 @@ class Post {
 
     }
 
+    public function create () {
+
+        $query = 'INSERT INTO ' . $this->table . '
+            SET
+             title = :title,
+             body = :body,
+             author = :author,
+             category_id = :category_id
+        ';
+
+        $stmt = $this->conexion->prepare($query);
+
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->body = htmlspecialchars(strip_tags($this->body));
+        $this->author = htmlspecialchars(strip_tags($this->author));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':body', $this->body);
+        $stmt->bindParam(':author', $this->author);
+        $stmt->bindParam(':category_id', $this->category_id);
+
+        if($stmt->execute()) {
+            return true;
+        }
+
+        printf("Error: %s.\n", $stmt->error);
+
+        return false;
+
+    }
+
 }
