80ddbcc7b4e4ce7a7156fe71aed302f67ea43eee
rgg7888 <rgg7888@outlook.com>
2021-07-08 22:02:49 -0600

    agregue el archivo de los recursos permitidos en la carpeta de configuracion

---
+++ api/v1.4/configuracion/RecursosPermitidos.php
@@ -0,0 +1,22 @@
+<?php
+
+#Recursos Permitidos
+
+$rp = [
+    'etiquetas' => [
+        'paginacion',
+        'registros'
+    ],
+    'atributos' => [
+        'paginacion',
+        'registros'
+    ],
+    'articulos' => [
+        'paginacion',
+        'registros'
+    ],
+    'categorias' => [
+        'paginacion',
+        'registros'
+    ]
+];
--- api/v1.4/v1.4.php
+++ api/v1.4/v1.4.php
@@ -1,28 +1,12 @@
 <?php
 
 require_once('./Respuesta/respuesta.php');
+require_once('./configuracion/RecursosPermitidos.php');
 
 $response = new Respuesta;
 
 //asignamos los recursos que seran permitidos en la api v1.4
-$response->setRp([
-    'etiquetas' => [
-        'paginacion',
-        'registros'
-    ],
-    'atributos' => [
-        'paginacion',
-        'registros'
-    ],
-    'articulos' => [
-        'paginacion',
-        'registros'
-    ],
-    'categorias' => [
-        'paginacion',
-        'registros'
-    ]
-]);
+$response->setRp($rp);
 
 //creamos la url en base ala uri que ingrese el usuario
 $response->setUrl();
