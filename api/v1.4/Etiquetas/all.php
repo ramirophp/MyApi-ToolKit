e687e1a1d1776eb37d5fdd4bdbcb7083559ef920
rgg7888 <rgg7888@outlook.com>
2021-07-08 22:14:36 -0600

    utilize caja para crear sus propias etiquetas

--- api/v1.4/Etiquetas/all.php
+++ api/v1.4/Etiquetas/all.php
@@ -1,5 +1,12 @@
 <?php
-require_once('./ht/Caja.php');
+/**
+ * La Caja tiene la funcionalidad 
+ * de Html pero no necesita de 
+ * una base de datos para funcionar
+ * y sus etiquetas tendra que 
+ * construirlas usted mismo
+ */
+#require_once('./ht/Caja.php');
 require_once('./ht/Html.php');
 require_once('./ht/Tags.php');
 require_once('./ht/const.php');
