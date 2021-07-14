d052f70a38f2e7fadcd0a8ab48bb29989eef0ffb
rgg7888 <rgg7888@outlook.com>
2021-07-14 09:30:15 -0600

    entorno de desarrollo

--- Configuracion/Paginacion.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//cambiar el numero 5 por los posts por pagina que usted quiera
-
-$articulos_por_pagina = 5;
-
-$inicia_desde = ($int > 1) ? ($int * $articulos_por_pagina - $articulos_por_pagina) : 0;
